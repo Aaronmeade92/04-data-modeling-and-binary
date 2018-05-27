d05ee3b4e171a3a50fc228d00bdfca73d762a5f2
Jozue06 <jozue06@gmail.com>
2018-05-26 19:04:06 -0700

    save

--- __test__/invert.spec.js
+++ __test__/invert.spec.js
@@ -2,27 +2,19 @@
 
 const parser = require('../lib/parsedBitmap.js');
 const readFile = require('../lib/readFile.js');
-
+const inverter = require('../lib/invert.js');
 describe('Parser Module', () => {
 
   it('construct bitmap from buffer', (done) => {
 
     readFile(`${__dirname}/../assets/bitmap.bmp`, (err, buffer) => {
       expect(err).toBeNull();
-      // let bmp = parser(buffer);
-      // expect(bmp).toBeDefined();
-      // expect(bmp).toBe('testing');
-      const bp = parser(buffer);
-      // expect(buffer).toBeDefined();
-      // expect(buffer.readInt32LE(28,30)).toBe(8);
+      const bp = inverter(buffer);
       expect(bp).toBeDefined();
       expect(typeof bp.dibHeader.width.readInt32LE(18,22) === 'number').toBe(true);
       
       done();
+
     });
   });
-
-  // it('should return console log', () => {
-
-  // })
 });
