334ffe94154966c95c739959b6b600791d0a539c
Jozue06 <jozue06@gmail.com>
2018-05-30 20:55:07 -0700

    changes file to white

--- __test__/writeFile.spec.js
+++ __test__/writeFile.spec.js
@@ -10,13 +10,15 @@ describe('Parser Module', () => {
 
     readFile(`/Users/joshuamcclung/codefellows/401/04-data-modeling-and-binary/assets/bitmap.bmp`, (err, buffer) => {
       // expect(err).toBeNull();
-      const bp = parser(buffer);
-      console.log(bp);
-      console.log(buffer);
-      const invertedImage = inverter(bp);//?
+      const bp = parser(buffer);//?
+      // console.log(bp);
+      // console.log(buffer);
+      const invertedImage = inverter(bp);
       console.log(invertedImage);
       expect(invertedImage).toBeDefined();
+
       writeFile(`${__dirname}/../assets/new/newBitmap.bmp`, invertedImage);
+      
       expect(typeof bp.dibHeader.width.readInt32LE(18,22) === 'number').toBe(true);
       
       done();
